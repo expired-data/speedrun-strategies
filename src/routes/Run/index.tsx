c9860f286c177905b5c4f27935c8462b064f4d03
A Ridley <expireddata@gmail.com>
2021-07-30 00:25:18 +0100

    feat: strats on run page

--- src/routes/Run/index.tsx
+++ src/routes/Run/index.tsx
@@ -1,21 +1,23 @@
-import React, { FC, RefObject, useMemo, useRef, useState } from "react";
+import React, { FC, RefObject, useRef, useState } from "react";
 import type { RouteComponentProps } from "react-router-dom";
 import ReactPlayer from "react-player";
-import { useGetData, getRun, useGetStrats, Strat } from "api";
-import { getScrollbarWidth } from "utils/useWindowSize";
-import { secondsToNice } from "utils/time";
+import { useGetData, getRun, useGetStrats } from "api";
+import styled from "styled-components";
+import { StratsDisplay } from "./StratsDisplay";
+import { AddStratForm } from "./AddStratForm";
 
 export interface Props {
   id: string;
 }
 
-const arrEq = (arr1: unknown[], arr2: unknown[]) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
+const FlexDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
-  return arr1.every((e, i) => e === arr2[i]);
-};
+const StyledReactPlayer = styled(ReactPlayer)`
+  flex: none;
+`;
 
 export const Run: FC<RouteComponentProps<Props>> = ({
   match: {
@@ -23,61 +25,40 @@ export const Run: FC<RouteComponentProps<Props>> = ({
   },
 }) => {
   const run = useGetData(getRun, id);
-  const scrollBarWidth = useMemo(getScrollbarWidth, []);
-  const [shownStrats, setShownStrats] = useState<Strat[]>();
-  const { strats, addStrat } = useGetStrats();
+  const { strats, addStrat, deleteStrat } = useGetStrats(run?.id || "");
+  const [time, setTime] = useState<number>(0);
   const ref = useRef<ReactPlayer>() as RefObject<ReactPlayer>;
-  const input = useRef() as RefObject<HTMLInputElement>;
   if (!run) {
     return <>Loading...</>;
   }
 
-  const updateStrats = (seconds: number): void => {
-    const relevant = strats.filter(
-      (strat) => Math.abs(seconds - strat.timestamp) < 60
-    );
-
-    if (
-      !shownStrats ||
-      !arrEq(
-        relevant.map((x) => x.id),
-        shownStrats.map((x) => x.id)
-      )
-    ) {
-      setShownStrats(relevant);
-    }
+  const updateTime = (time: number) => {
+    ref.current!.seekTo(time);
   };
 
   return (
     <div>
-      <ReactPlayer
-        url={run.videos.links[0]?.uri}
-        controls
-        width={`100%`}
-        height={`calc((80vw - 116px - ${scrollBarWidth}px)*${9 / 16})`}
-        ref={ref}
-        progressInternal={100}
-        onProgress={({ playedSeconds }) => updateStrats(playedSeconds)}
-        onPlay={() => updateStrats(ref.current!.getCurrentTime())}
-      />
-      <button
-        onClick={() => {
-          addStrat({
-            run: id,
-            timestamp: ref.current!.getCurrentTime(),
-            comment: input.current!.value,
-            id: Math.random().toString(),
-          });
-        }}
-      >
-        Add strat here
-      </button>
-      <input ref={input} placeholder={"strat comment"} />
-      {shownStrats?.map((strat) => (
-        <div>
-          {secondsToNice(strat.timestamp)} - {strat.comment}
-        </div>
-      ))}
+      <FlexDiv>
+        <StyledReactPlayer
+          url={run.videos.links[0]?.uri}
+          controls
+          width={`min(900px, 75%)`}
+          height={500}
+          ref={ref}
+          progressInternal={100}
+          onProgress={({ playedSeconds }: { playedSeconds: number }) =>
+            setTime(playedSeconds)
+          }
+          onPlay={() => setTime(ref.current!.getCurrentTime())}
+        />
+        <StratsDisplay
+          strats={strats}
+          time={time}
+          setTime={updateTime}
+          deleteStrat={deleteStrat}
+        />
+      </FlexDiv>
+      <AddStratForm time={time} run={run.id} addStrat={addStrat} />
     </div>
   );
 };
